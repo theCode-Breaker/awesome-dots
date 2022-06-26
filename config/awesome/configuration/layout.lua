4366281d0652e2c8521e8570f2079be34d2542ec
Adarsh <83777943+theCode-Breaker@users.noreply.github.com>
2022-06-27 00:31:40 +0530

    Add files via upload

--- config/awesome/configuration/layout.lua
+++ config/awesome/configuration/layout.lua
@@ -21,8 +21,8 @@ machi.editor.nested_layouts = {
 --- Set the layouts
 tag.connect_signal("request::default_layouts", function()
 	awful.layout.append_default_layouts({
-	    awful.layout.suit.floating,
 		awful.layout.suit.tile,
+		awful.layout.suit.floating,
 		centered,
 		mstab,
 		horizontal,
